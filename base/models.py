6c8979b80dfaeb5ad88656f43004f5f3f74e74cd
Thibaud Colas <thibaudcolas@gmail.com>
2023-08-11 14:06:36 +0100

    Add footer template partial with NavigationSettings model for social media links

--- base/models.py
+++ base/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from modelcluster.models import ClusterableModel
+from wagtail.admin.panels import (
+    FieldPanel,
+    MultiFieldPanel,
+)
+from wagtail.contrib.settings.models import (
+    BaseGenericSetting,
+    register_setting,
+)
 
-# Create your models here.
+
+@register_setting
+class NavigationSettings(ClusterableModel, BaseGenericSetting):
+    twitter_url = models.URLField(verbose_name="Twitter URL", blank=True)
+    github_url = models.URLField(verbose_name="GitHub URL", blank=True)
+    mastodon_url = models.URLField(verbose_name="Mastodon URL", blank=True)
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel("twitter_url"),
+                FieldPanel("github_url"),
+                FieldPanel("mastodon_url"),
+            ],
+            "Social settings",
+        )
+    ]
