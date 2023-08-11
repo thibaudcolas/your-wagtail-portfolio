7f8b9be0653ad39298a88feadea3d56c2cee3dec
Thibaud Colas <thibaudcolas@gmail.com>
2023-08-11 16:25:37 +0100

    Add a simple site menu implementation

--- base/templatetags/navigation_tags.py
+++ base/templatetags/navigation_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from wagtail.models import Site
 
 from base.models import FooterText
 
@@ -20,3 +21,10 @@ def get_footer_text(context):
     return {
         "footer_text": footer_text,
     }
+
+@register.simple_tag(takes_context=True)
+def get_site_root(context):
+    # This returns a core.Page. The main menu needs to have the site.root_page
+    # defined else will return an object attribute error ('str' object has no
+    # attribute 'get_children')
+    return Site.find_for_request(context["request"]).root_page
