376c6d674cb884c67fd507717b218350ebb1d514
Thibaud Colas <thibaudcolas@gmail.com>
2023-09-09 18:11:47 +0100

    Add portfolio page with basic StreamField blocks setup

---
+++ base/blocks.py
@@ -0,0 +1,60 @@
+from wagtail.blocks import (
+    CharBlock,
+    ChoiceBlock,
+    RichTextBlock,
+    StreamBlock,
+    StructBlock,
+)
+from wagtail.embeds.blocks import EmbedBlock
+from wagtail.images.blocks import ImageChooserBlock
+
+
+class ImageBlock(StructBlock):
+    """
+    Custom `StructBlock` for utilizing images with associated caption and
+    attribution data
+    """
+
+    image = ImageChooserBlock(required=True)
+    caption = CharBlock(required=False)
+    attribution = CharBlock(required=False)
+
+    class Meta:
+        icon = "image"
+        template = "base/blocks/image_block.html"
+
+
+class HeadingBlock(StructBlock):
+    """
+    Custom `StructBlock` that allows the user to select h2 - h4 sizes for headings
+    """
+
+    heading_text = CharBlock(classname="title", required=True)
+    size = ChoiceBlock(
+        choices=[
+            ("", "Select a heading size"),
+            ("h2", "H2"),
+            ("h3", "H3"),
+            ("h4", "H4"),
+        ],
+        blank=True,
+        required=False,
+    )
+
+    class Meta:
+        icon = "title"
+        template = "base/blocks/heading_block.html"
+
+
+class BaseStreamBlock(StreamBlock):
+    """
+    Define the custom blocks that `StreamField` will utilize
+    """
+
+    heading_block = HeadingBlock()
+    paragraph_block = RichTextBlock(icon="pilcrow")
+    image_block = ImageBlock()
+    embed_block = EmbedBlock(
+        help_text="Insert a URL to embed, for example https://www.youtube.com/watch?v=SGJFWirQ3ks",
+        icon="media",
+    )
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+from wagtail.models import Page
+from wagtail.fields import StreamField
+from wagtail.admin.panels import FieldPanel
 
-# Create your models here.
+from portfolio.blocks import PortfolioStreamBlock
+
+
+class PortfolioPage(Page):
+    """
+    A page to list our projects and skills.
+    """
+    parent_page_types = ["home.HomePage"]
+
+    body = StreamField(
+        PortfolioStreamBlock(),
+        blank=True,
+        use_json_field=True,
+        help_text="Use this section to list your projects and skills.",
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+    ]
